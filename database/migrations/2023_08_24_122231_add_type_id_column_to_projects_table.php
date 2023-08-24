e1b27fca070aa2ed0876472a548dbf30be215192
robertobenfini <roberto.benfini@gmail.com>
2023-08-24 14:24:09 +0200

    [add] creazione-fk-colonna

--- database/migrations/2023_08_24_122231_add_type_id_column_to_projects_table.php
+++ database/migrations/2023_08_24_122231_add_type_id_column_to_projects_table.php
@@ -14,7 +14,12 @@ return new class extends Migration
     public function up()
     {
         Schema::table('projects', function (Blueprint $table) {
-            //
+            //creo la nuova colonna nella tabella projects
+            $table->unsignedBigInteger('type_id')->nullable()->after('id');
+
+            //creo il vincolo/foreign key
+            $table->foreign('type_id')->references('id')->on('types')->onDelete('set null');
+
         });
     }
 
@@ -26,7 +31,8 @@ return new class extends Migration
     public function down()
     {
         Schema::table('projects', function (Blueprint $table) {
-            //
+            $table->dropForeign('projects_type_id_foreign');
+            $table->dropColumn('type_id');
         });
     }
 };
